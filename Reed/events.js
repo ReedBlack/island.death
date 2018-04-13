6d2fd7e6f29b420bf14a043afb1a76af4e600da6
REED BLACK <reedblack@REEDs-MacBook-Air.local>
2018-04-13 11:25:12 -0600

    reviews page

---
+++ Reed/events.js
@@ -0,0 +1,37 @@
+ [
+{type:"Busey Attack",
+bodyCount: 2,
+affectedPersons: "",
+description:"A naked, rabid, spear-wielding Gary Busey has emerged from the jungle and attacked"
+id:
+},
+{type:"Intergroup Feud"
+bodyCount: 1
+affectedPersons: "Tensions are rising! An argument turned into a fight turned into murder"
+id:
+},
+{type:"Falling Object"
+bodyCount: 1
+affectedPersons: ""
+description:"A coconut has fallen from a tree and killed someone. What luck!"
+id:
+},
+{type:"Boar Attack",
+bodyCount: 1,
+affectedPersons: "",
+description:"During your boar hunt, the boar got the upper-hand",
+id:
+},
+{type:"Monster Attack",
+bodyCount: 2,
+affectedPersons: "",
+description:"Went for a piss and never returned. Screams, followed by a deep gurgly noise were heard.",
+id:
+},
+{type:"'Chemicorp' Abduction",
+bodyCount: 1,
+affectedPersons: "",
+description:"The island's mysterious, dark corporate entity has abducted one from your party! Suspected cause of death: chemical weapons testing",
+id:
+},
+]
